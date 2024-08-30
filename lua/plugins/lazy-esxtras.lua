25f560177d1e5f0e20e45cb9fa5a4162e7ec0052
Mehedi Hasan <developerhasan99@gmail.com>
2024-08-31 05:51:25 +0600

    Laravel Intregrated

--- lua/plugins/lazy-esxtras.lua
+++
@@ -1,9 +0,0 @@
-return {
-  { import = "lazyvim.plugins.extras.lang.json" },
-  -- { import = "lazyvim.plugins.extras.lang.markdown" }, This Package failds at buildtime
-  { import = "lazyvim.plugins.extras.lang.toml" },
-  { import = "lazyvim.plugins.extras.lang.typescript" },
-  { import = "lazyvim.plugins.extras.lang.tailwind" },
-  { import = "lazyvim.plugins.extras.lang.php" },
-  { import = "lazyvim.plugins.extras.formatting.prettier" },
-}
